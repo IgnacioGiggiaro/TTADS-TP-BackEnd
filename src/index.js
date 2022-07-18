353767fe3aff1cd7b825b3c677a6f67207b38501
nacho <nachogiggiaro2000@gmail.com>
2022-07-18 16:55:55 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const morgan = require('morgan');
+//Initialiazations
+const app = express();
+require('./database');
+app.use(morgan('tiny'));
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    patialsDir: path.join(app.get('views'), 'partials'),
+    extname:'.hbs'
+}));
+app.set('view engine', '.hbs');
+//Middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret:'mysecretapp',
+    resave:true,
+    saveUninitialized:true
+}));
+//Global Variables
+
+//Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/paciente'));
+app.use(require('./routes/profesional'));
+app.use(require('./routes/schedule'));
+app.use(require('./routes/turno'));
+app.use(require('./routes/obraSocial'));
+app.use(require('./routes/practica'));
+//Static Files
+app.use(express.static(path.join(__dirname,'public')));
+//Server is listenning
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
