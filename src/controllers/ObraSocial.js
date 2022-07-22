384861df5a535507b26e08d29f05ccca8602d55f
nacho <nachogiggiaro2000@gmail.com>
2022-07-22 18:48:37 -0300

    Obra Social (Rutas, controladores)

--- src/controllers/ObraSocial.js
+++ src/controllers/ObraSocial.js
@@ -1,17 +1,17 @@
-const {ObraSocial} = require('../models/ObraSocial');
+const ObraSocial = require('../models/ObraSocial');
 
 const obraSocialController = {
     createObraSocial: async (req, res) => {
         try{
             const newObraSocial = new ObraSocial({
-              nombre: req.body.nombre,
+              nombre: req.body.nombre
             });
             await newObraSocial.save();
             return res.status(200).send({success:true, newObraSocial});
         } catch (error){
             return res
                 .status(500)
-                .send({ success: false, message: 'Error creating obra social'})
+                .send({ success: false, message: error})
         }
     },
 
@@ -25,20 +25,41 @@ const obraSocialController = {
                 .send({ success: false, message:'Error finding obra social'});
         }
     },
-    getObraSocial: async (req, res)=>{
-        try{
-            const {detailed} = req.query;
-            let obraSocial = null;
-            if(!detailed){
-                obraSocial=await ObraSocial.findById(req.params.id).exec();
-            }else{
-                const obraSocialModel = await ObraSocial.findById(req.params.id).populate({path:''})
+    getObraSocial: async (req, res) => {
+        try {
+            const obraSocial = await ObraSocial.findById(req.params.id).exec();
 
-            }
-        }catch (err){
+            if (!obraSocial) return res.status(404).send({ message: `There is no obra Social with ID: ${req.params.id}` });
 
+            return res.send(obraSocial);
+        } catch {
+            return res.status(500).send({ message: 'Error finding Obra Social' });
         }
-    }
+    },
+    deleteObraSocial: async (req, res) => {
+        try {
+            const removedObraSocial = await ObraSocial.findByIdAndRemove(req.params.id).exec();
+
+            if (!removedObraSocial) return res.status(404).send({ message: `There is no Obra Social with ID: ${req.params.id}` });
+
+            return res.send({message: "Obra Social deleted successfully"});
+        } catch {
+            return res.status(500).send({ message: 'Error deleting Obra Social' });
+        }
+    },
+    updateObraSocial: async (req, res) => {
+        try {
+            const updatedObraSocial = await ObraSocial.findByIdAndUpdate(req.params.id, {
+                nombre: req.body.nombre
+            }).exec();
+
+            if (!updatedObraSocial) return res.status(404).send({ message: `There is no obraSocial with ID: ${req.params.id}` });
+
+            return res.send({message: "Obra Social updated successfully"});
+        } catch {
+            return res.status(500).send({ message: 'Error updating Obra Social' });
+        }
+    },
 
 
 }
