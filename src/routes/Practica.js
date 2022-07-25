fbe35a51b23cfc675d888a17fed1e3126c0651bd
Tombolini Santiago <santitombo@gmail.com>
2022-07-25 20:09:27 -0300

    Route Practica

--- src/routes/Practica.js
+++ src/routes/Practica.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const controller = require('../controllers/Practica');
 const router = express.Router();
 
-router.get('/practica',(req, res)=>
-    {
-        res.send("Practica");
-    }
-);
+router.get('/PracticaGet/:id', controller.getPracticaById);
+router.get('/PracticaGet',controller.getPractica);
+router.post('/PracticaCreate', controller.createPractica);
+router.delete('/PracticaDelete/:id', controller.deletePractica);
+router.put('/PracticaUpdate/:id', controller.updatePractica);
+
 module.exports = router;
