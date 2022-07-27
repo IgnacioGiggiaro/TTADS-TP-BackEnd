9dc90d7089bd81d2484c35f8b904b0642430555a
Tombolini Santiago <santitombo@gmail.com>
2022-07-26 21:12:49 -0300

    DB connection remotely

--- src/database.js
+++ src/database.js
@@ -1,15 +1,6 @@
 const mongoose = require('mongoose');
 
-/*
-mongoose.connect('mongodb://localhost/notes-db-app', {
-    useCreateIndex:true,
-    useNewUrlParser:true,
-    useFindAndModify: false
-})
-    .then(db=> console.log('Db is connected'))
-    .catch(err => console.error(err));
 
- */
 mongoose.connect('mongodb+srv://admin:admin@hospital-db.gpzdy.mongodb.net/?retryWrites=true&w=majority',
     err => {
         if(err) throw err;
