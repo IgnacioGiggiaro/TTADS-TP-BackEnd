383fd1cb44c2dbd7a9092337d8b31fc4137472ba
nacho <nachogiggiaro2000@gmail.com>
2024-02-06 22:12:05 -0300

    TEst

--- src/controllers/controllers.test.js
+++ src/controllers/controllers.test.js
@@ -1,37 +1,21 @@
-const {turnoController, obraSocialController} = require("./Index");
+const {profesionalController, obraSocialController} = require("./Index");
 const ObraSocial = require('../models/ObraSocial')
+const ObraSocial = require('../models/Professional')
+const {Professional} = require("../models");
 
-
-
-test('Juego de perdedor', () => {
-    let OS= new ObraSocial  ;
+test('Obra Social Osde 720', async () => {
+    let OS = new ObraSocial;
     OS = await obraSocialController.getObraSocial("62dc6b30294deeae93787cef");
-
 
     const nombre = OS.nombre;
     expect(nombre).toBe('Osde 720');
 });
 
 // Prueba para un juego de todos 1
-test('Juego de todos 1', () => {
-    const juego = new Juego();
+test('Professional ', async () => {
+    let Prof = new Professional;
+    Prof = await profesionalController.getProfessionalByID("65bfaebb42c8662caae191b5");
 
-    for (let tiro = 0; tiro < 20; tiro++) {
-        juego.tirar(1); // El jugador derriba 1 pin en cada tiro
-    }
-
-    const puntuacion = juego.puntuacion();
-    expect(puntuacion).toBe(20);
-});
-
-test('Juego con al menos un Spare', () => {
-    const juego = new Juego();
-
-    juego.tirar(5); // Frame 1, lanzamiento 1: Se derriban 5 pinos
-    juego.tirar(5); // Frame 1, lanzamiento 2: Se derriban 5 pinos (Spare)
-    juego.tirar(3); // Frame 2, lanzamiento 1: Se derriban 3 pinos
-    juego.tirar(5);
-
-    const puntuacion = juego.puntuacion();
-    expect(puntuacion).toBe(21);
+    const nombre = Prof.nombre;
+    expect(nombre).toBe('Alfredo');
 });
